ab3462d16ae08b47161367b9ded513b26dd6e2c5
Oleg Roganin <94141484+olegroganin@users.noreply.github.com>
2022-01-15 16:53:31 +0200

    Add source code

---
+++ source/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Timer from './components/Timer';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Timer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ source/src/components/Timer.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import {useState, useEffect } from 'react';
+
+
+function Timer ()  {
+   const [seconds, setSeconds] = useState(0);
+   const [minutes, setMinutes] = useState(0);
+   const [hours, setHours] = useState(0);
+   const [run, setRun] = useState(false);
+
+   let timer;
+
+   useEffect(() => {
+	   if (run === true) {
+		   timer = setInterval(() => {
+			setSeconds(seconds+1)
+		if(seconds === 59) {
+			setMinutes(minutes+1);
+			setSeconds(0);
+		}
+		if(minutes === 59) {
+			setHours(hours + 1);
+			setMinutes(0);
+		}
+		   }, 1000)
+	   return () => clearInterval(timer);
+	   }
+   });
+
+   function start () {
+	   setRun(true);
+	   if(stop) {
+		   setSeconds(seconds + 1)
+	   }
+   };
+
+   function stop ()  {
+	   clearInterval(timer)
+   };
+
+   function reset () {
+      setSeconds(0);
+	   setMinutes(0);
+	   setHours(0);
+	   setRun(false);
+   };
+
+   return (
+      <div className='wrapper'>
+         <div className='container'>
+            <div className='timer'>
+               <h1>Timer</h1>
+               <h2>{hours < 10 ? '0' + hours : hours} : {minutes < 10 ? '0' + minutes : minutes} : {seconds < 10 ? '0' + seconds : seconds}</h2>
+            <div className='timer-buttons'>
+	            <button className='btn' onClick={start}>Start</button>
+               <button  className='btn' onClick={stop}>Stop</button>
+               <button className='btn' onClick={reset}>Reset</button>
+            </div>
+            </div>
+         </div>
+      </div>
+   )
+};
+
+export default Timer;
